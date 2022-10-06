4d5e7663a17e8371005dfd6044202113a76e6285
Bhanu Priya <113481902+bhanu-priyaa@users.noreply.github.com>
2022-10-06 19:13:44 +0530

    added files

---
+++ index.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+const QRcode = require("qrcode");
+var validate = require("uuid-validate");
+const S3_BUCKET = "vvplus";
+const s3bucket = new AWS.S3({ params: { Bucket: S3_BUCKET } });
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+  let response;
+  const userUuid = event.uuid;
+  if(!validate(userUuid)){
+    response = "Unvalid UUID";
+    return response;
+  }
+  const generatedQR = await QRcode.toDataURL(userUuid, {
+    errorCorrectionLevel: "H",
+  });
+  var filename = userUuid + ".png";
+  const fileExists = await s3
+    .headObject({
+      Bucket: S3_BUCKET,
+      Key: filename,
+    })
+    .promise()
+    .then(
+      () => true,
+      (err) => {
+        if (err.code == "NotFound") {
+          return false;
+        }
+        throw err;
+      }
+    );
+  if (fileExists) {
+    response = "already existed! " + filename.toString();
+  } else {
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: filename,
+      Body: generatedQR,
+      ACL: "public-read",
+      ContentEncoding: "base64",
+      ContentType: "image/png",
+    };
+    s3bucket.upload(params, function (err, data) {
+      if (err) {
+        response = "Error in uploading";
+      } else {
+        response = "Uploaded! " + filename.toString();
+      }
+    });
+  }
+  return response;
+};
